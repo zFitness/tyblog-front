e244bf236aaec7924f32594e8e845ab167a34ca1
zfitness <zhengmiaode@hotmail.com>
2020-10-29 10:33:50 +0800

    test:login

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -158,6 +158,7 @@ export default {
           this.loading = true
           this.$store.dispatch('user/login', this.loginForm)
             .then(() => {
+              console.log(this.otherQuery)
               this.$router.push({ path: this.redirect || '/', query: this.otherQuery })
               this.loading = false
             })
